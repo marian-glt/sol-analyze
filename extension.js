75f6a91895b7b1612d3921bbe8fc3030e2d838d5
Marian Galatanu <38663778+mariang8090@users.noreply.github.com>
2021-12-07 13:01:27 +0000

    Attempt at connecting ext and parser

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const parser = require('@solidity-parser/parser');
+const fs = require('fs');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,7 +10,7 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -19,12 +21,39 @@ function activate(context) {
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('sol-analyze.execute', function () {
 		// The code you place here will be executed every time your command is executed
-
+		//Attempt at getting open workspace folder to then get the file that should be passed through the plugin
+		let message = '';
+		if(vscode.workspace.workspaceFolders !== undefined){
+			let file = vscode.window.activeTextEditor.document.uri.fsPath;
+			let ext = file.split('.').pop();
+			ext === 'sol' ? callParser(file): messenger('Cannot execute on a non-Solidity file, please try again.');
+		} else {
+			messenger("Sol-Analyzer: Working folder not found, open a folder then execute again.");
+		}
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello sol-analyze!');
+		vscode.window.showInformationMessage(message);
 	});
 
 	context.subscriptions.push(disposable);
+}
+function messenger(text){
+	vscode.window.showInformationMessage(text);
+}
+
+function callParser(filePath){
+	messenger(`Sol-Analyzer executing on: ${filePath}`);
+	messenger('before');
+    fs.readFile(filePath, 'utf-8', function(err, data){
+        try {
+            const ast = parser.parse(data);
+            
+        } catch (e) {
+            if (e instanceof parser.ParserError) {
+                console.error(e.errors)
+            }
+        }
+    });
+    messenger('after');
 }
 
 // this method is called when your extension is deactivated
--- parser.js
+++ parser.js
@@ -1,16 +1,22 @@
 const parser = require('@solidity-parser/parser');
 
 const input = `
-    contract test {
-        uint256 a;
-        function f() {}
+    pragma solidity 0.8.10;
+    contract SimpleStorage {
+        uint storedData;
+        function set(uint x) public {
+            storedData = x;
+        }
+        function get() public view returns (uint) {
+            return storedData;
+        }
     }
 `
 try {
-    const ast = parser.parse(input)
-    console.log(ast)
+    const ast = parser.parse(input, { loc: true })
+    console.log(ast);
 } catch (e) {
     if (e instanceof parser.ParserError) {
-        console.error(e.errors)
+        console.error(e.errors);
     }
 }
