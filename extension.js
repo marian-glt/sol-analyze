0166ea54fb46ac85815692f7ebd5892c2a25518c
Marian Galatanu <38663778+mariang8090@users.noreply.github.com>
2021-12-20 12:12:42 +0000

    small changes

--- extension.js
+++ extension.js
@@ -1,8 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const reader = require('fs');
 const parser = require('@solidity-parser/parser');
-const fs = require('fs');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -19,15 +19,15 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('sol-analyze.execute', function () {
+	let disposable = vscode.commands.registerCommand('sol-analyze.exec', function () {
 		// The code you place here will be executed every time your command is executed
 		let message = '';
 		if(vscode.workspace.workspaceFolders !== undefined){
 			let file = vscode.window.activeTextEditor.document.uri.fsPath;
 			let ext = file.split('.').pop();
-			ext === 'sol' ? callParser(file): messenger('Cannot execute on a non-Solidity file, please try again.');
+			ext === 'sol' ? parse(file): messenger('Sol-Analyzer: Cannot execute on a non-Solidity file, please try again.');
 		} else {
-			messenger("Sol-Analyzer: Working folder not found, open a folder then execute again.");
+			messenger("Sol-Analyzer: Working folder not found, open a folder then try again.");
 		}
 		// Display a message box to the user
 		vscode.window.showInformationMessage(message);
@@ -40,11 +40,12 @@ function messenger(text){
 	vscode.window.showInformationMessage(text);
 }
 
-function callParser(filePath){
+function parse(filePath){
 	messenger(`Sol-Analyzer executing on: ${filePath}`);
-	fs.readFile(filePath, 'utf-8', function(err, data){
+
+	reader.readFile(filePath, 'utf-8', function(err, code){
 		try {
-			const ast = parser.parse(data);
+			const ast = parser.parse(code);
 			console.log(ast);
 		} catch (e) {
 			if (e instanceof parser.ParserError) {
