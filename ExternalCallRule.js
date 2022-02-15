a42c929583849a31def69117db85660b385e6810
Marian Galatanu <38663778+mariang8090@users.noreply.github.com>
2022-02-15 20:06:25 +0000

    completed 2nd external call scenario

--- ExternalCallRule.js
+++ ExternalCallRule.js
@@ -7,43 +7,100 @@ const ExternalCallRule = (ast) => {
 function findCall(ast) {
     parser.visit(ast, {
         FunctionDefinition : function(node){
-            findLonelyCall(node.body);
+            goodCase(node.body);
         }
     })
 }
 /**
- * @function findLonelyCall — Looks for any instance in the source code where a transaction function may be called on its own.
+ * @function worstCase — Looks for any instance in the source code where a transaction function may be called on its own.
  * e.g. _to.transfer(msg.value)
  * @param {import("@solidity-parser/parser/dist/src/ast-types").Block} block 
  * Block contains all statements that can be part of a method in the case of this application.  
  * */
-function findLonelyCall(block){
-    let functionCall;
-    const lonelyCallFindings = []
+function worstCase(block){
+    let functionCall = null;
+    const wanted = ['call', 'callcode', 'delegatecall', 'send', 'transfer']
+    const worstCaseFindings = []
     parser.visit(block, {
         ExpressionStatement : function(exp_node){
             if(exp_node.expression['type'] === 'FunctionCall' && exp_node.expression['expression'].type === 'MemberAccess'){
                 functionCall = exp_node.expression['expression'];
-                if(externalCall(functionCall.memberName)){
+                if(externalCall(wanted, functionCall.memberName)){
                     const searchResult = {
                         'function' : functionCall.memberName,
                         'start' : functionCall.loc.start,
                         'end' : functionCall.loc.end,
                     }
-                    lonelyCallFindings.push(searchResult);
+                    worstCaseFindings.push(searchResult);
                 }
             }
         }
     })
-    return lonelyCallFindings;
+    return worstCaseFindings;
+}
+
+function goodCase(block){
+    const wanted = ['callcode', 'delegatecall', 'send', 'transfer']
+    const goodCaseFindings = []
+    parser.visit(block, {
+        VariableDeclarationStatement : function(vd_node){
+            let boolean = null;
+            parser.visit(vd_node, {
+                VariableDeclaration : function(variable){
+                    if(variable.typeName['name'] === 'bool'){
+                        boolean = variable;
+                    }
+                }
+            })
+
+            if(boolean != null && vd_node.initialValue.type === 'FunctionCall'){
+                let functionCall = null;
+                parser.visit(vd_node.initialValue, {
+                    FunctionCall : function(node){
+                        const call = node.expression
+                        externalCall(wanted, call['memberName']) ? functionCall = call : null
+                    }
+                })
+
+                if(functionCall != null){
+                    parser.visit(block, {
+                        ExpressionStatement : function(exp_node){
+                            if(exp_node.expression.type === 'FunctionCall'){
+                                const exp_sub_node = exp_node.expression;
+                                if(exp_sub_node.expression['name'] === 'require'){
+                                    const argument = exp_node.expression['arguments']
+                                    if(argument[0]['name'] == boolean['name']){
+                                        console.log("This is ok, but using call instead of %s is better", functionCall['memberName']);
+                                        //underline the function call line
+                                    } else {
+                                        const searchResult = {
+                                            'function' : functionCall['memberName'],
+                                            'start' : functionCall.loc.start,
+                                            'end' : functionCall.loc.end,
+                                        };
+
+                                        goodCaseFindings.push(searchResult);
+                                    }
+                                }
+                            }
+                        }
+                    })
+                }
+            }
+        }
+    })
+}
+
+function bestCase(block){
+
 }
 /**
  * Check if the function call passed is any of the possible functions that need an external check.
+ * @param {Array} possibleCalls - Array of function calls to look out for
  * @param {String} memberName - The function called in the analyzed statement
  * @returns True - If the function called is one of Ethereum's transfer functions.
  */
-function externalCall(memberName){
-    const possibleCalls = ['call', 'callcode', 'delegatecall', 'send', 'transfer'];
+function externalCall(possibleCalls, memberName){
     let matches = false;
     possibleCalls.forEach(element => {
         memberName === element ? matches = true : null;
