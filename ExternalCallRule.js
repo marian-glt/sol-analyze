180e3ca1d2ddd6d9a888959185a7d285ac3fb80c
Marian Galatanu <38663778+mariang8090@users.noreply.github.com>
2022-02-15 16:47:56 +0000

    more work on external call checks

--- ExternalCallRule.js
+++ ExternalCallRule.js
@@ -1,17 +1,54 @@
 const parser = require("@solidity-parser/parser")
 
 const ExternalCallRule = (ast) => {
-    const contracts = extractContracts(ast);
+    findCall(ast);
 }
 
-//extract contracts in file to avoid passing the whole AST throughout the execution
-const extractContracts = (ast) => {
-    var contracts = []
+function findCall(ast) {
     parser.visit(ast, {
-        ContractDefinition : function(node){
-            contracts.push(node);
+        FunctionDefinition : function(node){
+            console.log(node);
         }
     })
+}
+/**
+ * @function findLonelyCall — Looks for any instance in the source code where a transaction function may be called on its own.
+ * e.g. _to.transfer(msg.value)
+ * @param {import("@solidity-parser/parser/dist/src/ast-types").Block} block 
+ * Block contains all statements that can be part of a method in the case of this application.  
+ * */
+function findLonelyCall(block){
+    let functionCall;
+    let searchResult;
+    let lonelyCallFindings = []
+    parser.visit(block, {
+        ExpressionStatement : function(exp_node){
+            if(exp_node.expression['type'] === 'FunctionCall' && exp_node.expression['expression'].type === 'MemberAccess'){
+                functionCall = exp_node.expression['expression'];
+                if(externalCall(functionCall.memberName)){
+                    searchResult = {
+                        'functionCalled' : functionCall.memberName,
+                        'start' : functionCall.loc.start,
+                        'end' : functionCall.loc.end
+                    }
+                    lonelyCallFindings.push(searchResult);
+                }
+            }
+        }
+    })
+    console.log(lonelyCallFindings.length);
+    return lonelyCallFindings;
+}
 
-    return contracts;
+const externalCall = (memberName) =>{
+    const possibleCalls = ['call', 'callcode', 'delegatecall', 'send', 'transfer'];
+    let matches = false;
+    for (const call in possibleCalls) {
+        memberName === call ? matches = true : null;
+    }
+
+    return matches;
+}
+module.exports = {
+    ExternalCallRule
 }
