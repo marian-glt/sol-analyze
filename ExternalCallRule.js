a78e82857123699dbd41f4aae41bf0a41f1ac4ab
Marian Galatanu <38663778+mariang8090@users.noreply.github.com>
2022-02-15 23:07:23 +0000

    completed 3rd case scenario check

--- ExternalCallRule.js
+++ ExternalCallRule.js
@@ -7,7 +7,7 @@ const ExternalCallRule = (ast) => {
 function findCall(ast) {
     parser.visit(ast, {
         FunctionDefinition : function(node){
-            goodCase(node.body);
+            bestCase(ast);
         }
     })
 }
@@ -38,7 +38,11 @@ function worstCase(block){
     })
     return worstCaseFindings;
 }
-
+/**
+ * @function goodCase Looks for any instance in the source code where a transaction function is called AND checked using require,
+ * but the developer doesn't use 'call' as the function for the transaction to be performed, which is the recommended way of doing it.
+ * @param {import("@solidity-parser/parser/dist/src/ast-types").Block} block 
+ */
 function goodCase(block){
     const wanted = ['callcode', 'delegatecall', 'send', 'transfer']
     const goodCaseFindings = []
@@ -92,7 +96,55 @@ function goodCase(block){
 }
 
 function bestCase(block){
+    const bestCaseFindings = []
+    parser.visit(block, {
+        VariableDeclarationStatement : function(vd_node){
+            let boolean = null;
+            parser.visit(vd_node, {
+                VariableDeclaration : function(variable){
+                    if(variable.typeName['name'] === 'bool'){
+                        boolean = variable;
+                    }
+                }
+            })
 
+            if(boolean != null && vd_node.initialValue.type === 'FunctionCall'){
+                let functionCall = null;
+                parser.visit(vd_node.initialValue, {
+                    NameValueExpression : function(nve_node){
+                        if(nve_node.expression['type'] === 'MemberAccess' &&
+                            nve_node.expression['memberName'] === 'call'){
+                                functionCall = nve_node.expression;
+                        }
+                    }
+                })
+
+                if(functionCall != null){
+                    parser.visit(block, {
+                        ExpressionStatement : function(exp_node){
+                            if(exp_node.expression.type === 'FunctionCall'){
+                                const exp_sub_node = exp_node.expression;
+                                if(exp_sub_node.expression['name'] === 'require'){
+                                    const argument = exp_node.expression['arguments']
+                                    if(argument[0]['name'] == boolean['name']){
+                                        console.log("It workssss")
+                                        const searchResult = {
+                                            'function' : functionCall['memberName'],
+                                            'start' : functionCall.loc.start,
+                                            'end' : functionCall.loc.end,
+                                        };
+
+                                        bestCaseFindings.push(searchResult);
+                                        console.log(bestCaseFindings)
+                                    }
+                                }
+                            }
+                        }
+                    })
+                }
+            }
+        }
+    })
 }
 /**
  * Check if the function call passed is any of the possible functions that need an external check.
