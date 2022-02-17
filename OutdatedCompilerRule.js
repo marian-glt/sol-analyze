c53b4609e95440a380c3cc30081d5df451a928bf
Marian Galatanu <38663778+mariang8090@users.noreply.github.com>
2022-02-17 18:44:20 +0000

    fixes

--- OutdatedCompilerRule.js
+++ OutdatedCompilerRule.js
@@ -1,15 +1,15 @@
 const parser = require("@solidity-parser/parser");
 
-const OutdatedCompilerRule = (ast) => {
+function OutdatedCompilerRule(ast){
 	let importCase = null;
 	parser.visit(ast, 
 		{
 			PragmaDirective: function(node){
 				const pragma = node.value;
-				const isUsingOperators = find(pragma, operators);
-				if(isUsingOperators[0] === '^'){
+				const isUsingOperators = find(pragma, operators(undefined));
+				if(isUsingOperators.includes('^')){
 					importCase = checkCompilerVersion(pragma);
-				} else if(operators(pragma)){
+				} else{
 					importCase = checkForTwoOperators(pragma);
 				}
 			}
@@ -19,11 +19,12 @@ const OutdatedCompilerRule = (ast) => {
 	return reportFindings(importCase);
 }
 
-const checkCompilerVersion = (version) =>{
+function checkCompilerVersion(version){
+	console.log(version);
 	if(isNewVersion(version)){
-		return'>=0.8.0'
+		return '>=0.8.0';
 	} else if(isOldVersion(version)){
-		return '<0.8.0'
+		return '<0.8.0';
 	} else {
 		return 'invalid';
 	}
@@ -40,22 +41,14 @@ const operators = (operator) =>{
 	return operands;
 }
 
-const isOldVersion = (version) =>{
-	const oldVersions = '(0\.)+((1\.[0-7])|' + 
-							   '(2\.[0-2])|' + 
-							   '(3\.[0-6])|' + 
-							   '(4\.\b([0-9]|1[0-9]|2[0-6])\b)|' + 
-							   '(5\.\b([0-9]|1[0-7])\b)|' + 
-							   '(6\.\b([0-9]|1[0-2])\b)| '+ 
-							   '(7\.[0-6])){1}'
+function isOldVersion(version){
+	const oldVersions = '(0\.)+((1\.[0-7])|(2\.[0-2])|(3\.[0-6])|(4\.\b([0-9]|1[0-9]|2[0-6])\b)|(5\.\b([0-9]|1[0-7])\b)|(6\.\b([0-9]|1[0-2])\b)|(7\.[0-6])){1}'
 	const regex = new RegExp(oldVersions);
-
 	return regex.test(version);
 }
 
 const isNewVersion = (version) =>{
-	const newVersions = new RegExp('(0\.(8\.\b([0-9]|1[0-2])\b)');
-
+	const newVersions = new RegExp('(0\.)+(8\.\b([0-9]|1[0-2])\b)');
 	return newVersions.test(version);
 }
 
@@ -63,7 +56,6 @@ const checkForTwoOperators = (pragma) =>{
 	const hasOperators = operators(pragma);
 	if(hasOperators){
 		const usedOperators = find(pragma, operators(undefined));
-
 		/** In case the developer specifies a range of compiler versions for which the contract can execute.
 		 * For example, pragma solidity >0.6.0 <=0.8.11 */
 		if(usedOperators.length === 2){
@@ -98,7 +90,7 @@ const find = (str, regex) => {
 const reportFindings = (results) =>{
 	let message = null;
 	let isUsingOldCompiler = null;
-	if(results.constructor === String){
+	if(typeof results === 'string'){
 		if(results === '>=0.8.0'){
 			message = "Good! The contract is using a newer version of the compiler.";
 			isUsingOldCompiler = false;
