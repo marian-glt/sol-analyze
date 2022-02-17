be0a6de27675cc3b66905277099754b503b39a96
Marian Galatanu <38663778+mariang8090@users.noreply.github.com>
2022-02-17 18:21:09 +0000

    small changes

--- VisibilityRule.js
+++ VisibilityRule.js
@@ -1,11 +1,11 @@
 const parser = require("@solidity-parser/parser");
 
-const VisibilityRule = (ast) =>{
+function VisibilityRule(ast){
     parser.visit(ast, {
         FunctionDefinition : function(node){
             if(!node.isFallback && !node.isConstructor){
                 if(node.visibility === 'default'){
-                    console.log("You may have a function with default visibility at line: " + node.loc.start.line);
+                    console.log("Your function " + node.name + " has default visibility at line: " + node.loc.start.line);
                 }
             }
         }
