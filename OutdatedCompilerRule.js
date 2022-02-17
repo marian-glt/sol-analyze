be910b318b22e6cf8bc6b579d5f4e3a80b4643fb
Marian Galatanu <38663778+mariang8090@users.noreply.github.com>
2022-02-17 15:59:05 +0000

    Completed refactoring of compiler rule

--- OutdatedCompilerRule.js
+++ OutdatedCompilerRule.js
@@ -1,72 +1,80 @@
-// @ts-nocheck
-const parser = require("@solidity-parser/parser")
+const parser = require("@solidity-parser/parser");
 
 const OutdatedCompilerRule = (ast) => {
-	let isOldVersion = false;
+	let importCase;
 	parser.visit(ast, 
 		{
 			PragmaDirective: function(node){
 				const pragma = node.value;
-				const matches = find(node.value, opRegEx);
-				
-				if(matches.includes('^') && oldVerRegEx(pragma)){
-					//for contracts that use the "^" sign, e.g. "pragma solidity ^0.7.0"
-					isOldVersion = true;
-				} else if(opRegEx(pragma)){
-					const signMatches = find(pragma, opRegEx());
-					if(signMatches.length === 2){
-						//for contracts that have 2 limitation signs, e.g. "pragma solidity >0.6.0 <=0.8.0"
-						pragma.replace(opRegEx, '');
-						const firstHalf = pragma.substring(0, pragma.indexOf(signMatches[1]))
-						const secondHalf = pragma.substring(pragma.indexOf(signMatches[1]));
-						const pragmaArr = [firstHalf, secondHalf];
-						for (let i = 0; i < pragmaArr.length; i++) {
-							oldVerRegEx(pragmaArr[i]) ? isOldVersion = true : null;
-						}
-					} else if(signMatches.length === 1 && oldVerRegEx(pragma)){
-						//for contracts with one limitation sign e.g. "pragma solidity ^0.8.11"
-						isOldVersion = true;
-					} else {
-						//for contracts that do not have a limitation sign, but just the solidity version specified e.g. "pragma solidity 0.6.0"
-						oldVerRegEx(pragma) ? isOldVersion = true : null;
-					}
+				const matches = find(pragma, operators);
+				if(matches.length === 1 && matches[0] === '^'){
+					importCase = checkCompilerVersion(pragma);
+				} else if(operators(pragma)){
+					importCase = checkForTwo(pragma);
 				}
 			}
 		}
 	)
 
-	return isOldVersion;
+	return importCase;
 }
-const opRegEx = (value) =>{
-	const operands = new RegExp('[>|<]+=?|\\^', 'g');
 
+const checkCompilerVersion = (version) =>{
+	if(isNewVersion(version)){
+		return'>=0.8.0'
+	} else if(isOldVersion(version)){
+		return '<0.8.0'
+	} else {
+		return 'invalid';
+	}
+}
+
+const operators = (operator) =>{
+	const operands = new RegExp('[>|<]+=?|\\^', 'g');
 	const illegalOperands = new RegExp('=>|=<', 'g');
 
-	if(typeof value !== "undefined"){
-		return (operands.test(value) && !illegalOperands.test(value))
+	if(typeof operator !== "undefined"){
+		return (operands.test(operator) && !illegalOperands.test(operator));
 	} 
 	
 	return operands;
 }
 
-const oldVerRegEx = (value) =>{
-	const oldVersions = new RegExp('(0\.(1\.[0-7])|' + 
-								'(2\.[0-2])|' + 
-								'(3\.[0-6])|' + 
-								'(4\.\b([0-9]|1[0-9]|2[0-6])\b)|' + 
-								'(5\.\b([0-9]|1[0-7])\b)|' + 
-								'(6\.\b([0-9]|1[0-2])\b)|' + 
-								'(7\.[0-6])){1}');
-	return oldVersions.test(value);
+const isOldVersion = (version) =>{
+	const oldVersions = '(0\.)+((1\.[0-7])|' + 
+							   '(2\.[0-2])|' + 
+							   '(3\.[0-6])|' + 
+							   '(4\.\b([0-9]|1[0-9]|2[0-6])\b)|' + 
+							   '(5\.\b([0-9]|1[0-7])\b)|' + 
+							   '(6\.\b([0-9]|1[0-2])\b)| '+ 
+							   '(7\.[0-6])){1}'
+
+	const regex = new RegExp(oldVersions);
+
+	return regex.test(version);
 }
 
-const newVerRegEx = (value) =>{
+const isNewVersion = (version) =>{
 	const newVersions = new RegExp('(0\.(8\.\b([0-9]|1[0-2])\b)');
 
-	return newVersions.test(value);
+	return newVersions.test(version);
 }
-const checkPragmaValues = (pragma) =>{
-	pragma.replace()
+
+const checkForTwo = (pragma) =>{
+	const hasOperators = operators(pragma);
+	if(hasOperators){
+		const usedOperators = find(pragma, operators(undefined));
+		/** In case the developer specifies a range of compiler versions for which the contract can execute.
+		 * For example, pragma solidity >0.6.0 <=0.8.11 */
+		if(usedOperators.length === 2){
+			pragma = pragma.replace(operators(undefined), '');
+			const usedVersions = pragma.split(' ', 2);
+			const versionsStatus = []
+			usedVersions.forEach(version => {
+				versionsStatus.push(checkCompilerVersion(version));
+			});
+		}
+	}
 }
 
 const find = (str, regex) => {
