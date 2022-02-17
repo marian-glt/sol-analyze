0f6c160c16c570e0761726c196b2b2123bb453c5
Marian Galatanu <38663778+mariang8090@users.noreply.github.com>
2022-02-17 13:28:22 +0000

    refactoring function that checks compiler version

--- OutdatedCompilerRule.js
+++ OutdatedCompilerRule.js
@@ -1,35 +1,34 @@
+// @ts-nocheck
 const parser = require("@solidity-parser/parser")
 
 const OutdatedCompilerRule = (ast) => {
-    const opReg = new RegExp('[>|<]+=?|\\^', 'g');
-	const verReg = new RegExp('(0\.(1\.[0-7])|(2\.[0-2])|(3\.[0-6])|(4\.[1-2]?[0-9])|(5\.1?[0-9])|(6\.1?[0-9])|(7\.[0-6])){1}');
 	let isOldVersion = false;
 	parser.visit(ast, 
 		{
 			PragmaDirective: function(node){
 				const pragma = node.value;
-				const matches = find(node.value, opReg);
+				const matches = find(node.value, opRegEx);
 				
-				if(matches.includes('^') && verReg.test(pragma)){
+				if(matches.includes('^') && oldVerRegEx(pragma)){
 					//for contracts that use the "^" sign, e.g. "pragma solidity ^0.7.0"
 					isOldVersion = true;
-				} else if(opReg.test(pragma)){
-					const signMatches = find(pragma, opReg);
+				} else if(opRegEx(pragma)){
+					const signMatches = find(pragma, opRegEx());
 					if(signMatches.length === 2){
 						//for contracts that have 2 limitation signs, e.g. "pragma solidity >0.6.0 <=0.8.0"
-						pragma.replace(opReg, '');
+						pragma.replace(opRegEx, '');
 						const firstHalf = pragma.substring(0, pragma.indexOf(signMatches[1]))
 						const secondHalf = pragma.substring(pragma.indexOf(signMatches[1]));
 						const pragmaArr = [firstHalf, secondHalf];
 						for (let i = 0; i < pragmaArr.length; i++) {
-							verReg.test(pragmaArr[i]) ? isOldVersion = true : null;
+							oldVerRegEx(pragmaArr[i]) ? isOldVersion = true : null;
 						}
-					} else if(signMatches.length === 1 && verReg.test(pragma)){
+					} else if(signMatches.length === 1 && oldVerRegEx(pragma)){
 						//for contracts with one limitation sign e.g. "pragma solidity ^0.8.11"
 						isOldVersion = true;
 					} else {
 						//for contracts that do not have a limitation sign, but just the solidity version specified e.g. "pragma solidity 0.6.0"
-						verReg.test(pragma) ? isOldVersion = true : null;
+						oldVerRegEx(pragma) ? isOldVersion = true : null;
 					}
 				}
 			}
@@ -37,6 +36,37 @@ const OutdatedCompilerRule = (ast) => {
 	)
 
 	return isOldVersion;
+}
+const opRegEx = (value) =>{
+	const operands = new RegExp('[>|<]+=?|\\^', 'g');
+
+	const illegalOperands = new RegExp('=>|=<', 'g');
+
+	if(typeof value !== "undefined"){
+		return (operands.test(value) && !illegalOperands.test(value))
+	} 
+	
+	return operands;
+}
+
+const oldVerRegEx = (value) =>{
+	const oldVersions = new RegExp('(0\.(1\.[0-7])|' + 
+								'(2\.[0-2])|' + 
+								'(3\.[0-6])|' + 
+								'(4\.\b([0-9]|1[0-9]|2[0-6])\b)|' + 
+								'(5\.\b([0-9]|1[0-7])\b)|' + 
+								'(6\.\b([0-9]|1[0-2])\b)|' + 
+								'(7\.[0-6])){1}');
+	return oldVersions.test(value);
+}
+
+const newVerRegEx = (value) =>{
+	const newVersions = new RegExp('(0\.(8\.\b([0-9]|1[0-2])\b)');
+
+	return newVersions.test(value);
+}
+const checkPragmaValues = (pragma) =>{
+	pragma.replace()
 }
 
 const find = (str, regex) => {
